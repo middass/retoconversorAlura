e4c8d9218857edaed26070f721acfb04e0369b96
middass <36519746+middass@users.noreply.github.com>
2024-04-17 12:55:34 -0500

    Add files via upload

---
+++ conversormonedas/src/Main.java
@@ -0,0 +1,26 @@
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("**********************");
+        System.out.println("Lee con cuidado las opciones de conversión");
+
+        System.out.println("1) Dolar =>> Peso argentino");
+        System.out.println("2) Peso Argenino =>> DOlar");
+        System.out.println("3) Dolar =>> Real brasileño");
+        System.out.println("4) Real brasileño =>> Dolar");
+        System.out.println("5) Dolar =>> Peso colombiano");
+        System.out.println("6) Peso colombiano =>> Dolar");
+        System.out.println("7) Salir");
+
+        System.out.println("Elija una opción válida");
+
+
+
+
+    }
+}
